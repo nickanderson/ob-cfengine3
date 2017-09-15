bc77c42afc84dd76b9b34dd7752b3ac2522215a6
Nick Anderson <nick@cmdln.org>
2017-09-14 20:39:28 -0500

    Cleanups from code review
    - Add license info
    - escape filename with shell-quote-argument
    - use PATH for cf-agent location
    - Re-indention so that (delete-file tempfile) is more apparently inside
    of unwind-protect
    - Remove autoloads and require ob and org-mode

--- ob-cfengine3.el
+++ ob-cfengine3.el
@@ -1,15 +1,38 @@
 ;;; ob-cfengine3.el --- Org Babel functions for CFEngine 3
 
+;; Copyright (C) 2017  Nick Anderson
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;; THE SOFTWARE.
+
+;; Author: Nick Anderson <nick@cmdln.org>
+;; Keywords: tools, convenience
+;; URL: https://github.com/nickanderson/ob-cfengine3
+;; Version: 0.0.1
+
+
 ;;; Commentary:
 ;; Execute CFEngine 3 policy inside org-mode src blocks.
 
-;; Author: Nick Anderson <nick@cmdln.org>
-;; URL: https://github.com/nickanderson/ob-cfengine3
-;; Version: 0.0.1
-;;
 ;;; Code:
+(require 'org)
+(require 'ob)
 
-(defvar ob-cfengine3-command "/var/cfengine/bin/cf-agent"
+(defvar ob-cfengine3-command "cf-agent"
   "Name of command to use for executing cfengine policy.")
 
 (defvar ob-cfengine3-command-options ""
@@ -18,9 +41,8 @@ Note that --file will be appended to the options.")
 
 (defvar ob-cfengine3-file-control-stdlib "body file control{ inputs => { '$(sys.libdir)/stdlib.cf' };}\n"
   "File control body to include the standard libriary from $(sys.libdir).
-It is usefult to inject into an example source block before execution.")
+It is useful to inject into an example source block before execution.")
 
-;;;###autoload
 (defconst ob-cfengine3-header-args-cfengine3
   '(
     (no-lock . :any)
@@ -29,7 +51,6 @@ It is usefult to inject into an example source block before execution.")
     (bundlesequence . :any))
   "CFEngine specific header arguments.")
 
-;;;###autoload
 (defun org-babel-execute:cfengine3 (body params)
   "Actuate a block of CFEngine 3 policy.
 This function is called by `org-babel-execute-src-block'.
@@ -49,25 +70,22 @@ This function is called by `org-babel-execute-src-block'.
         (when include-stdlib (insert ob-cfengine3-file-control-stdlib))
         (insert body))
       (unwind-protect
-      (shell-command-to-string
-      (concat
-        ob-cfengine3-command
-        " "
-        (when bundlesequence (concat "--bundlesequence "  bundlesequence ))
-        " "
-        (when define (concat "--define "  define ))
-        " "
-        (unless use-locks "--no-lock")
-        ob-cfengine3-command-options
-        " "
-        (format " --file %s" tempfile)))
-    (delete-file tempfile))))
+         (shell-command-to-string
+           (concat
+            ob-cfengine3-command
+            " "
+            (when bundlesequence (concat "--bundlesequence "  bundlesequence ))
+            " "
+            (when define (concat "--define "  define ))
+            " "
+            (unless use-locks "--no-lock")
+            ob-cfengine3-command-options
+            " "
+            (format " --file %s" (shell-quote-argument tempfile))))
+        (delete-file tempfile))))
 
-;;;###autoload
-(eval-after-load "org"
-  '(add-to-list 'org-src-lang-modes '("cfengine3" . cf3))
-  '(add-to-list 'org-babel-tangle-lang-exts '("cfengine3" . "cf")))
+(add-to-list 'org-src-lang-modes '("cfengine3" . cfengine3))
+(add-to-list 'org-babel-tangle-lang-exts '("cfengine3" . "cf"))
 
 (provide 'ob-cfengine3)
-
 ;;; ob-cfengine3.el ends here
