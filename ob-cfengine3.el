807661e3c06109dc637f293b21076cf851bd1312
Nick Anderson <nick@cmdln.org>
2019-05-20 14:25:05 -0500

    ob-cfengine3 0.0.6
    New header args:
    - run-with-main : automatically wrap the contents of SRC block in bundle
    agent __main__ before execution.
    - tangle-with-main : automatically wrap the contents of SRC block in
    bundle agent __main__ before tangeling.
    - auto-main : shortcut for enabling both tangle-with-main and
    run-with-main
    
    Changes:
    - command-in-result-filename now defaults to the value of :tangle if set

--- ob-cfengine3.el
+++ ob-cfengine3.el
@@ -22,7 +22,7 @@
 ;; Author: Nick Anderson <nick@cmdln.org>
 ;; Keywords: tools, convenience
 ;; URL: https://github.com/nickanderson/ob-cfengine3
-;; Version: 0.0.5
+;; Version: 0.0.6
 
 ;;; Commentary:
 ;; Execute CFEngine 3 policy inside org-mode src blocks.
